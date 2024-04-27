74d9aaf11eaf3d1a573cdf2b249106a22b36089f
LucasLandimLLL <lucas.landim2510@gmail.com>
2024-04-26 21:53:16 -0300

    tomar no cu man

--- app/controllers/produtos_controller.ts
+++ app/controllers/produtos_controller.ts
@@ -1,10 +1,14 @@
 // import type { HttpContext } from '@adonisjs/core/http'
 
-export default class ProdutosController {
+import Produto from "../models/produto.js"
 
+export default class ProdutosController {
+    
     async index(){
+        return await Produto.all()
+    }
+    async store (){
         return [1,2,3]
     }
-    
 }
 
